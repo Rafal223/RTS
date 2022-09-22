8f074f7ddea84b81480a47cab23c7de9d01a9b51
Rafal233 <rafimaka45@gmail.com>
2022-09-22 11:05:44 +0200

    sql

---
+++ rts.sql
@@ -0,0 +1,17 @@
+INSERT INTO players (name,email,password) VALUES ("Adam","adamP@gmail.com","123");
+INSERT INTO players (name,email,password) VALUES ("Rafał","rafal2@gmail.com","456");
+INSERT INTO players (name,email.password) VALUES ("Jacek","Jacekszef@wp.pl","789aaa");
+INSERT INTO players (name,email.password) VALUES ("Pawel","paweltu@wp.pl","dergaaa");
+
+INSERT INTO resources (name, description) VALUES ("drewno","pali sie");
+INSERT INTO resources (name, description) VALUES ("glina","jest szara");
+INSERT INTO resources (name, description) VALUES ("żelazo","błyszczy sie");
+
+INSERT INTO troops (name, description, damage, distance_damage, defence) VALUES ("łucznik","strzela","1","15","5");
+INSERT INTO troops (name, description, damage, distance_damage, defence) VALUES ("miecznik","ora pole","10","0","10");
+INSERT INTO troops (name, description, damage, distance_damage, defence) VALUES ("kusznik","bełtuje","3","30","15");
+INSERT INTO troops (name, description, damage, distance_damage, defence) VALUES ("rycerz","ora pole lepiej","20","15","25");
+
+INSERT INTO buildings (name) VALUES ("doł z gliny");
+INSERT INTO buildings (name) VALUES ("huta żelaza");
+INSERT INTO buildings (name) VALUES ("chata drwala");
